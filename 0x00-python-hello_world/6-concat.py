67c0f7f27d441686b6f6e5867f003858913307db
sebsibe23 <sebsibe1990@proton.me>
2023-10-02 13:18:47 -0700

    q6

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-strinstitution = "Holberton"
-strbranch = "School"
-strinstitution += " " + strbranch
-print("Welcome to {}!".format(strinstitution))
+str1 = "Holberton"
+str2 = "School"
+str1 += " " + str2
+print(f"Welcome to {str1}!")
