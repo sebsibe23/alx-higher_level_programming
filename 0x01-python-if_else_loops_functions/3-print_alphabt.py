e3a5d1bfa35c987ff3f558b6e09630c9a3ffabd8
sebsibe23 <sebsibe1990@proton.me>
2023-10-03 10:27:49 -0700

    q3 final push

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for charletter in range(97, 123):
-    if chr(charletter) is not 'q' and chr(charletter) is not 'e':
-        print("{}".format(chr(charletter)), end="")
+for c in range(97, 123):
+    if c not in [101, 113]:
+        print(chr(c), end="")
