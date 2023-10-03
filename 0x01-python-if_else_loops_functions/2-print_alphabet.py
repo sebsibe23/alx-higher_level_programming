7b98964132cefc9602dbed68718217ca1ec09bf0
sebsibe23 <sebsibe1990@proton.me>
2023-10-03 10:11:50 -0700

    q3 s

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 # 2-print_alphabet.py
-print("".join(chr(i) for i in range(97, 123)))
+
+"""Print alphabet in lowercase, not followed with a new line."""
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
