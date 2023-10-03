e7ec523800f3213a7c7d93615b5d5b4d72c62624
sebsibe23 <sebsibe1990@proton.me>
2023-10-03 10:42:01 -0700

    q4

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 4-print_hexa.py
+
+"""Print a anumbers 0 to 98 in decimal and hexadecimal format."""
+for numindec in range(0, 99):
+    print("{} = {}".format(numindec, hex(numindec)))
