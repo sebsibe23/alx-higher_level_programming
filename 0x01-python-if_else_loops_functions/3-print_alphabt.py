278bf167f797880f6bc6b6068f93fd8afb7d4e39
sebsibe23 <sebsibe1990@proton.me>
2023-10-03 10:31:41 -0700

    q3 second push

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for c in range(97, 123):
-    if c not in [101, 113]:
-        print(chr(c), end="")
+for ch in range(97, 123):
+    if chr(ch) is not 'q' and chr(ch) is not 'e':
+        print("{}".format(chr(ch)), end="")
