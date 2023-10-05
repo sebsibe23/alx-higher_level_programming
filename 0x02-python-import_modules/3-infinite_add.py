998507def2b49fa7cb20ba3819ca57c377d5e196
sebsibe23 <sebsibe1990@proton.me>
2023-10-05 10:54:54 -0700

    q3 q

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
+#!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-
-    strtot = 0
-    for i in range(len(sys.argv) - 1):
-        strtot += int(sys.argv[i + 1])
-    print("{}".format(strtot))
+    import sys, math
+    intstrresult = 0
+    for i in sys.argv:
+        intstrresult += int(i)
+        print("{}".format(intstrresult))
