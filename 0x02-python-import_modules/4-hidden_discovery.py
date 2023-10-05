720f88c83a3bf7e8625b4e45027e787d7c8d27db
sebsibe23 <sebsibe1990@proton.me>
2023-10-05 11:16:13 -0700

    hidden text

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+import hidden_4
 
 if __name__ == "__main__":
-    import hidden_4
-
-    strnames_ = dir(hidden_4)
-    for strnames_ in names:
-        if strnames_[:2] != "__":
-            print(strnames_)
+    t = dir(hidden_4)
+    t = [q for q in t if not q.startswith('__')]
+    t.sort()
+    for q in t:
+        print(q)
