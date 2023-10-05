88ad7d67119bb22119315aaf539b31779520f069
sebsibe23 <sebsibe1990@proton.me>
2023-10-05 11:42:23 -0700

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-
-
-def magic_calculation(e, h):
+def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
-
-    if e < h:
-        z = add(e, h)
-        for s in range(4, 6):
-            z = add(z, s)
-        return (z)
-
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
     else:
-        return(sub(e, h))
+        return sub(a, b)
