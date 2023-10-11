dceae322b62c3b1a6a30400b05260f350605771a
sebsibe23 <sebsibe1990@proton.me>
2023-10-11 09:55:29 -0700

    q12 second

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,40 +1,25 @@
 #!/usr/bin/python3
-def subtract_func(varint_list):
-    strsubtract_val = 0
-    max_val = max(varint_list)
-
-    for num in varint_list:
-        if max_val > num:
-            strsubtract_val += num
-
-    return (max_val - strsubtract_val)
-
-
-def roman_to_integer(varroman_str):
-    if not varroman_str:
+def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
         return 0
 
-    if not isinstance(varroman_str, str):
-        return 0
+    roman_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
 
-    varroman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    dict_keys = list(varroman_dict.keys())
+    strint_val = 0
+    strprev = 0
 
-    int_val = 0
-    strl_roman = 0
-    varint_list = [0]
-
-    for char in varroman_str:
-        for roman_num in dict_keys:
-            if roman_num == char:
-                if varroman_dict.get(char) <= strl_roman:
-                    int_val += subtract_func(varint_list)
-                    varint_list = [varroman_dict.get(char)]
-                else:
-                    varint_list.append(varroman_dict.get(char))
-
-                strl_roman = varroman_dict.get(char)
-
-    int_val += subtract_func(varint_list)
-
-    return (int_val)
+    for i in roman_string:
+        strcurr = roman_dict[i]
+        strint_val += strcurr
+        if strprev and strprev < strcurr:
+            strint_val -= strprev * 2
+        strprev = strcurr
+    return strint_val
