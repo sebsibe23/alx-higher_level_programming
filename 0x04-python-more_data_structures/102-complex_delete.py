9b9d82c53f1932bcd87dee6d3f61dc36a0e0ddb4
sebsibe23 <sebsibe1990@proton.me>
2023-10-11 10:22:11 -0700

    add 102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    """Delete keys in the dictionary with the specific value."""
-    varkeys_to_d = [var_key for var_key, str_val in a_dictionary.items() if str_val == value]
-    for var_key in varkeys_to_d:
-        del a_dictionary[var_key]
-    return a_dictionary
+    varlist_k = list(a_dictionary.keys())
+
+    for val_dic in varlist_k:
+        if value == a_dictionary.get(val_dic):
+            del a_dictionary[val_dic]
+
+    return (a_dictionary)
