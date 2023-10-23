e55012337f9bffdde18b15c3b31b1dd632ef40d1
sebsibe23 <sebsibe1990@proton.me>
2023-10-23 10:16:41 -0700

    q1 rev

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
     x (int): The number of elements to print. Default is 0.
     Returns:
     int: a real number of elements printed.
-    """ 
+    """
     varcount = 0
     varo_put = ""
     for g in range(x):
