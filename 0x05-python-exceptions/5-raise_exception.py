0f58a8c69c34e0d3fcceb10fdd592c1e09ef6c7e
sebsibe23 <sebsibe1990@proton.me>
2023-10-23 11:54:55 -0700

    q6

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def raise_exception():
+    """
+    raise_exception
+    a function raises a TypeError exception.
+    Returns:
+    None
+    """
+    message = "Exception raised"
+    raise TypeError(message)
