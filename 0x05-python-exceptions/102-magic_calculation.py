05d3e1e146ca058b1e119756a29151eeeec9565e
sebsibe23 <sebsibe1990@proton.me>
2023-10-23 12:43:34 -0700

    102 rev

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-def magical_computation(a, b):
+def magic_calculation(a, b):
     """
     magical_computation
         a function performs the "magical" computation on two numbers.
-        It first initializes the result variable to 0. 
+        It first initializes the result variable to 0.
         Then it runs the loop from 1 to 2,
         and for each iteration, if a current index is greater than a,
         it raises an exception.
-        If not, it adds a integer division of a power b 
+        If not, it adds a integer division of a power b
         and the current index to the result.
-        If an exception occurs, it simply passes 
+        If an exception occurs, it simply passes
         and continues to the next line.
-        Finally, it adds a and b to the 
+        Finally, it adds a and b to the
         result and returns it.
-    Parameters: 
+    Parameters:
         a: The first integer number.
         b: The second integer number.
 
