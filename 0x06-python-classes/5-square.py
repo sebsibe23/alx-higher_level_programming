c9a1d4676fd639ae0c64b8fe223a85e61e3387c7
sebsibe23 <sebsibe1990@proton.me>
2023-10-24 10:57:30 -0700

    last sub

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,48 +1,45 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 """Module that defines a class Square"""
 
+
 class Square:
-    """A class that encapsulates the concept of a square.
+    """
+    A class that encapsulates the concept of a square: (based on 4-square.py).
 
     Attributes:
-        __size (int): The length of one side of the square.
+        size: The length of one side of the square.
     """
-
     def __init__(self, size=0):
         """Constructor for the Square class.
 
         Args:
-            size (int): The length of one side of the square. Defaults to 0.
+            size: The length of one side of the square. Defaults to 0.
         """
         self.__size = size
 
     def area(self):
         """Method to compute the area of the square.
 
-        Returns:
-            int: The area of the square.
+        Returns: The area of the square.
         """
         return self.__size ** 2
 
     @property
     def size(self):
-        """Getter for the size attribute.
-
-        Returns:
-            int: The size of the square.
+        """Returns the size of a square
         """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Setter for the size attribute.
+        """Getter for the size attribute.
 
         Args:
-            value (int): The new size of the square.
+            value (int): The new size of the square..
 
         Raises:
-            TypeError: If `value` is not an integer.
-            ValueError: If `value` is less than 0.
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -52,10 +49,9 @@ class Square:
             self.__size = value
 
     def my_print(self):
-        """Prints a visual representation of the square with the '#' character.
-
-        If the size is 0, it prints an empty line.
+        """Prints a visual representation of the square with the '#' character
         """
+
         if self.__size == 0:
             print()
         for i in range(self.__size):
