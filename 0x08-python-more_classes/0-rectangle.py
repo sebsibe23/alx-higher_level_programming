f219aeed9258fbab831eeda570d6cae9b70cbd3a
sebsibe23 <sebsibe1990@proton.me>
2023-10-30 02:35:29 -0700

    q0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,9 +2,8 @@
 """Module 0-rectangle, built for a Python project.
 """
 
+
 class Rectangle:
-    """This is an empty class named Rectangle. 
-    It doesn't contain any attributes or methods yet, but it can be expanded upon in future tasks.
+    """This is an empty class named Rectangle.
     """
     pass
-
