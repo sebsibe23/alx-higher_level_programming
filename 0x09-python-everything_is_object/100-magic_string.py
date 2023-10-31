73e1311183fbafac0137fb43be463a65d20b1cb7
sebsibe23 <sebsibe1990@proton.me>
2023-10-31 11:08:43 -0700

    q100 final submit

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
+
+
 def magic_string():
     """
-    Function Name: magic_string
-    Parameters: None
-
-    This function returns a string "BestSchool" repeated n times, where n is the number of times the function has been called. 
-    It uses a counter attribute on the function object itself to keep track of the number of times it has been called.
+    This function returns a string "BestSchool" repeated n times,
     """
     magic_string.counter = getattr(magic_string, 'counter', -1) + 1
     return ", ".join(["BestSchool"] * (magic_string.counter + 1))
