008986c4c6b4726b65b5a0c27a8ed163fd527be9
sebsibe23 <sebsibe1990@proton.me>
2023-10-31 11:14:49 -0700

    100 magic last

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-
 def magic_string():
-    """
-    This function increments a counter attribute 'xt'.
-    It then returns the string "BestSchool" .
-    """
     magic_string.t = getattr(magic_string, 't', 0) + 1
     return ("BestSchool, " * (magic_string.t - 1) + "BestSchool")
