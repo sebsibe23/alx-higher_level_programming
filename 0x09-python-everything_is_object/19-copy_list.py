846a2028ac68d788ba793681ee52c441c15973b0
sebsibe23 <sebsibe1990@proton.me>
2023-10-31 11:19:44 -0700

    q19 last submit

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-def copy_list(a):
-    """
-    Function Name: copy_list
-    Parameter:
-    a (list): The list to be copied.
-
-    This function returns a copy of the input list 'a' using the built-in 'copy()' method.
-    """
-    return a.copy()
-
+def copy_list(u):
+    return u.copy()
