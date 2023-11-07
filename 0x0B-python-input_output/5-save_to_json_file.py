51f11750a90e33a9af1687176d961ce657a985cf
sebsibe23 <sebsibe1990@proton.me>
2023-11-07 10:17:31 -0800

    all error fixed

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""the save to jason module"""
+
 
 import json
 
+
 def save_to_json_file(my_obj, filename):
+    """a function that writes an Object to a text file, using a JSON"""
     with open(filename, "w") as f:
         json.dump(my_obj, f)
