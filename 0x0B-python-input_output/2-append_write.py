bf20364ad58c9428b516754cad7f038d468502a5
sebsibe23 <sebsibe1990@proton.me>
2023-11-07 09:31:22 -0800

    q3

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
+"""
+Function to append a given text into a file.
+"""
 
 
 def append_write(filename="", text=""):
     """
-    Function to append a given text into a file.
-    Parameters:
-    filename (str): The name of the file to append to.
-    If the file does not exist, it will be created.
-    text (str): The text to append into the file.
-    Returns:
     int: The number of characters appended into the file.
     """
     with open(filename, "a", encoding="UTF-8") as f:
