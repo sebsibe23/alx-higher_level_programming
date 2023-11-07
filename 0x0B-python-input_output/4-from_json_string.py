78ecf8ab8ea4a0d30a26a14eedda95294228039a
sebsibe23 <sebsibe1990@proton.me>
2023-11-07 09:54:36 -0800

    q5 last

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""
+Function to convert a JSON formatted string to a Python object.
+"""
 
 
 import json
 
 
-def from_json_string(my_str):
-    return json.loads(my_str)
+def to_json_string(my_obj):
+    """Python object: The Python object represented by the JSON string."""
+    return json.dumps(my_obj)
