178831dbda7e73a0e48819f5aaf0cc911e325068
sebsibe23 <sebsibe1990@proton.me>
2023-11-07 08:54:36 -0800

    l

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,5 @@
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as fl:
-        print(fl.read(), end="")
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read(), end="")
