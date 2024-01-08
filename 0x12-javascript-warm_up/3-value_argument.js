f377f42b2dba07e9d3bfbb95c7564c78ef0a9a7b
sebsibe23 <sebsibe1990@proton.me>
2024-01-08 09:22:45 -0800

    q3 last

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,8 +2,10 @@
 
 // Conditional Statement
 // Description: This block of code checks if process.
-// argv[2] is undefined and logs a corresponding message to the console. 
-// If process.argv[2] is defined, it logs the value of process.argv[2].
+// argv[2] is undefined and logs a corresponding
+// message to the console.
+// If process.argv[2] is defined,
+// it logs the value of process.argv[2].
 if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
