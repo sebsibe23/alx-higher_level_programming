ceb16ed49e02abcac82f51a24258837073d439ba
sebsibe23 <sebsibe1990@proton.me>
2024-01-08 08:45:49 -0800

    q1

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
+
+// Variable: myfirstVar
+// Description: This variable holds the string 'JavaScript is amazing'.
 const myfirstVar = 'JavaScript is amazing';
+
+// Output: myfirstVar
+// Description: This line logs the value
+// of the variable myfirstVar to the console.
 console.log(myfirstVar);
