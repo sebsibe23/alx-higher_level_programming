3a3ce682e89efc910979396bc9b0aab5254da148
sebsibe23 <sebsibe1990@proton.me>
2024-01-08 10:41:57 -0800

    q9 and q10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 /*
- * This Node.js script defines a recursive function named 'factorial' that takes 
- * a single parameter 'n'. If 'n' is less than 0, it returns -1. If 'n' is 0 or 
- * not a number, it returns 1. Otherwise, it returns the product of 'n' and the 
- * factorial of 'n-1'. The script then calls the 'factorial' function with the 
- * first command-line argument (excluding the default ones) converted to a number 
+ * This Node.js script defines a recursive function named 'factorial' that takes
+ * a single parameter 'n'. If 'n' is less than 0, it returns -1. If 'n' is 0 or
+ * not a number, it returns 1. Otherwise, it returns the product of 'n' and the
+ * factorial of 'n-1'. The script then calls the 'factorial' function with the
+ * first command-line argument (excluding the default ones) converted to a number
  * and prints the result.
  */
 
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
 
-// Function: add
-// Description: This function takes two parameters,
-// a and b, and calculates their sum.
-// It assigns the sum to a variable called tot
-// and logs it to the console.
-function add(a, b) {
-  const tot = a + b;
-  console.log(tot);
+/*
+ * This Node.js script defines a function named 'add' that takes two parameters
+ * 'a' and 'b'. It calculates the sum of 'a' and 'b', and prints the result.
+ * The script then calls the 'add' function with the first two command-line
+ * arguments (excluding the default ones) converted to numbers.
+ */
+
+function add (a, b) {
+  const c = a + b;
+  console.log(c);
 }
 
-// Function Call: add
-// Description: This line calls the add function,
-// passing the numeric values of process.argv[2]
-// and process.argv[3] as arguments.
 add(Number(process.argv[2]), Number(process.argv[3]));
