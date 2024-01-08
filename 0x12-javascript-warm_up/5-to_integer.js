98e1a3a2f0db0a9618cfefaaf237df4b86399efb
sebsibe23 <sebsibe1990@proton.me>
2024-01-08 08:07:32 -0800

    3rd

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 let args = process.argv.slice(2);
 let num = Number(args[0]);
 if (isNaN(num)) {
-    console.log("Not a number");
+    console.log('Not a number');
 } else {
-    console.log("My number: " + Math.floor(num));
+    console.log('My number: ' + Math.floor(num));
 }
