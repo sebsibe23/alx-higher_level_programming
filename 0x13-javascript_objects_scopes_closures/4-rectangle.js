a3646e45788e7453b64b3535c4f19ffa3bdb481a
sebsibe23 <sebsibe1990@proton.me>
2024-01-09 04:14:42 -0800

    q4 final

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,50 +1,57 @@
 #!/usr/bin/node
 
 /**
- * Class representing a rectangle.
+ * This is a Rectangle class in Node.js.
+ * It has a constructor and three methods:
+ * print, rotate, and double.
+ *
+ * Constructor (w, h): Initializes a new Rectangle
+ * object if both width (w) and
+ * height (h) are positive numbers.
+ *
+ * print(): Prints a visual representation of
+ * the Rectangle object using 'X'
+ * characters. The number of 'X' characters in
+ * a row corresponds to the width,
+ * and the number of rows corresponds to the height.
+ *
+ * rotate(): Swaps the width and the height
+ * of the Rectangle object.
+ *
+ * double(): Doubles the width and the height
+ * of the Rectangle object.
+ *
+ * The Rectangle class is exported as a module.
  */
+
 class Rectangle {
-	/**
-	 * Create a new rectangle instance with positive width and height.
-	 * @param {number} w - The width of the rectangle.
-	 * @param {number} h - The height of the rectangle.
-	 */
-	constructor (w, h) {
-		if (w > 0 && h > 0) {
-			this.width = w;
-			this.height = h;
-		}
-	}
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	/**
-	 * Print a visual representation of the rectangle using 'X' characters.
-	 */
-	print () {
-		for (let i = 0; i < this.height; i++) {
-			let s = '';
-			for (let j = 0; j < this.width; j++) {
-				s += 'X';
-			}
-			console.log(s);
-		}
-	}
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
 
-	/**
-	 * Rotate the rectangle by swapping its width and height.
-	 */
-	rotate () {
-		const aux = this.width;
-		this.width = this.height;
-		this.height = aux;
-	}
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
 
-	/**
-	 * Double the width and height of the rectangle.
-	 */
-	double () {
-		this.width *= 2;
-		this.height *= 2;
-	}
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
