fb1534abcaed89fca38e6152a046599c7bb3f112
sebsibe23 <sebsibe1990@proton.me>
2024-01-09 04:01:39 -0800

    q4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -4,47 +4,47 @@
  * Class representing a rectangle.
  */
 class Rectangle {
-  /**
-   * Create a new rectangle instance with positive width and height.
-   * @param {number} w - The width of the rectangle.
-   * @param {number} h - The height of the rectangle.
-   */
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+	/**
+	 * Create a new rectangle instance with positive width and height.
+	 * @param {number} w - The width of the rectangle.
+	 * @param {number} h - The height of the rectangle.
+	 */
+	constructor (w, h) {
+		if (w > 0 && h > 0) {
+			this.width = w;
+			this.height = h;
+		}
+	}
 
-  /**
-   * Print a visual representation of the rectangle using 'X' characters.
-   */
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += 'X';
-      }
-      console.log(s);
-    }
-  }
+	/**
+	 * Print a visual representation of the rectangle using 'X' characters.
+	 */
+	print () {
+		for (let i = 0; i < this.height; i++) {
+			let s = '';
+			for (let j = 0; j < this.width; j++) {
+				s += 'X';
+			}
+			console.log(s);
+		}
+	}
 
-  /**
-   * Rotate the rectangle by swapping its width and height.
-   */
-  rotate () {
-    const aux = this.width;
-    this.width = this.height;
-    this.height = aux;
-  }
+	/**
+	 * Rotate the rectangle by swapping its width and height.
+	 */
+	rotate () {
+		const aux = this.width;
+		this.width = this.height;
+		this.height = aux;
+	}
 
-  /**
-   * Double the width and height of the rectangle.
-   */
+	/**
+	 * Double the width and height of the rectangle.
+	 */
 	double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
+		this.width *= 2;
+		this.height *= 2;
+	}
 }
 
 module.exports = Rectangle;
