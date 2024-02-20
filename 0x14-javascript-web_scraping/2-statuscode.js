b9a7628bac5c7b9dcf87cacf8bf91ad12858e350
sebsibe23 <sebsibe1990@proton.me>
2024-02-20 03:48:59 -0800

    q2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -17,14 +17,7 @@
  * - Error: Raised when an unexpected error occurs during the API request or response parsing.
  * - TypeError: Raised when the film ID is not provided as a command line argument.
  */
-const request = require('request')
-
-const url = process.argv[2]
-
-request.get(url, function (error, response) {
-  if (error) {
-    console.error(error)
-  } else {
-    console.log('code:', response.statusCode)
-  }
-})
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
