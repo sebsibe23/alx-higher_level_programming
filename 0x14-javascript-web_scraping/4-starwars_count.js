ee5b1188e9499486f1ac4a0a8a3a01e4dc2a06ee
sebsibe23 <sebsibe1990@proton.me>
2024-02-20 09:51:05 -0800

    q4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -23,28 +23,27 @@
  *   - node starwars-count-with-character.js https://swapi-api.alx-tools.com/api/films
  *
  * Dependencies:
-  - request module: Used to make HTTP requests to the Star Wars API.
+ * - request module: Used to make HTTP requests to the Star Wars API.
  **/
+const request = require('request');
+const starWarsURL = process.argv[2];
+let times = 0;
 
-const request = require('request')
-const starWarsUri = process.argv[2]
-let times = 0
-
-request(starWarsUri, function (_err, _res, body) {
-  body = JSON.parse(body).results
+request(starWarsURL, function (_err, _res, body) {
+  body = JSON.parse(body).results;
 
   for (let i = 0; i < body.length; ++i) {
-    const characters = body[i].characters
+    const characters = body[i].characters;
 
     for (let j = 0; j < characters.length; ++j) {
-      const character = characters[j]
-      const characterId = character.split('/')[5]
+      const character = characters[j];
+      const characterId = character.split('/')[5];
 
       if (characterId === '18') {
-        times += 1
+        times += 1;
       }
     }
   }
 
-  console.log(times)
-})
+  console.log(times);
+});
