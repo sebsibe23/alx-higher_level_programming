2317831a1df4b1331e5f88f8a999324918312dad
sebsibe23 <sebsibe1990@proton.me>
2024-02-20 03:03:34 -0800

    q0 fixed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,18 @@
 #!/usr/bin/node
+
+/**
+ * File Reader Script
+ *
+ * Description:
+ * This script reads the content of a file specified as a command-line argument
+ * and logs it to the console. It utilizes the 'fs' module from Node.js to
+ * perform the file reading operation asynchronously. If an error occurs during
+ * the file reading process, the error message is logged. Otherwise, the file
+ * content is printed to the console.
+ */
+
 const fs = require('fs')
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (err) {
-    console.log(err)
-  } else {
-    process.stdout.write(data)
-  }
+
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content)
 })
