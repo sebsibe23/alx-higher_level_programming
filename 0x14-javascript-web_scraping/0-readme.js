f99b22be5e59e30d8d7305453fd314b02e827939
sebsibe23 <sebsibe1990@proton.me>
2024-02-20 10:04:41 -0800

    q0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,44 @@
 #!/usr/bin/node
+
+/**
+ * File Reader
+ * -----------
+ *
+ * Description: This script reads the content of a file and prints it to the console.
+ *
+ * Author: sebsibe solomon
+ *
+ * Date: 02202024
+ *
+ * Usage: node file_reader.js [file_path]
+ *
+ * Exceptions:
+ * - Error: Raised when an error occurs during the file reading process.
+ * - TypeError: Raised when the file path is not provided as a command line argument.
+ *
+ * Dependencies:
+ * - fs module: Used to read the content of the file.
+ *
+ * Parameters:
+ * - file_path: A string representing the path to the file.
+ */
+
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    console.log(content);
+const filePath = process.argv[2];
+
+try {
+  if (!filePath) {
+    throw new TypeError('Error: File path is missing. Please provide a file path as a command line argument.');
   }
-});
+
+  fs.readFile(filePath, 'utf8', function (error, content) {
+    if (error) {
+      throw error;
+    } else {
+      console.log(content);
+    }
+  });
+} catch (error) {
+  console.error(error);
+}
