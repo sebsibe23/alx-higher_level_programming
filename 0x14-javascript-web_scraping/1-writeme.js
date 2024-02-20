b9401f22fc8c869d484f8f46d04659ab3b95fd74
sebsibe23 <sebsibe1990@proton.me>
2024-02-20 10:07:43 -0800

    q1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,26 @@
 #!/usr/bin/node
-const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-  if (error) console.log(error);
-});
+
+const fs = require('fs')
+
+const filePath = process.argv[2]
+const content = process.argv[3]
+
+try {
+  if (!filePath) {
+    throw new TypeError('Error: File path is missing. Please provide a file path as a command line argument.')
+  }
+
+  if (!content) {
+    throw new TypeError('Error: Content is missing. Please provide content to write as a command line argument.')
+  }
+
+  fs.writeFile(filePath, content, (error) => {
+    if (error) {
+      throw error
+    } else {
+      console.log('Content has been written to the file successfully.')
+    }
+  })
+} catch (error) {
+  console.error(error)
+}
