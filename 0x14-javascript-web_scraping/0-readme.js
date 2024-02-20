83c9529c744272e791bd38362106ad668874415f
sebsibe23 <sebsibe1990@proton.me>
2024-02-20 02:57:20 -0800

    q1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,27 +1,5 @@
 #!/usr/bin/node
-/**
- * File Reader
- * ------------
- *
- * Description: This script reads a file and
- * outputs its contents to the console.
- *
- * Author: sebsibe solomon
- *
- * Date: February 20, 2024
- *
- * Usage: node file-reader.js [file_path]
- *
- * Exceptions:
- * - Error: Raised when an unexpected error
- *   occurs during file reading.
- * - TypeError: Raised when the file path is
- *   not provided as a command line argument.
- * - ENOENT: Raised when the file specified by
- *   the file path does not exist.
- */
 const fs = require('fs')
-
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err)
